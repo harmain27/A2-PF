767e142f14be4c5a4cd1ced54a1e12ccf3076326
harmain27 <harmainnoor5166@gmail.com>
2025-07-10 00:32:31 +0500

    Create Practice 05.cpp

---
+++ Practice 05.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//using namespace std;
+//
+//bool prime(int n) {
+//    if (n < 2)
+//        return false;
+//    for (int i = 2; i * i <= n; i++) {
+//        if (n % i == 0)
+//            return false;
+//    }
+//    return true;
+//}
+//
+//int main() {
+//    int a;
+//    cout << "Enter a number to check\n";
+//    cin >> a;
+//    if (prime(a)) {
+//        cout << a << " is a prime number\n";
+//    }
+//    else {
+//        cout << a << " is not a prime number\n";
+//    }
+//    return 0;
+//}
