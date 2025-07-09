e3e04da6ebccbea82f2e3bb1c10e16588091f02c
harmain27 <harmainnoor5166@gmail.com>
2025-07-10 00:32:15 +0500

    Create Practice 01.cpp

---
+++ Practice 01.cpp
@@ -0,0 +1,14 @@
+////Sum of numbers 1 to N
+//#include<iostream>
+//using namespace std;
+//int sumOneToN(int n) {
+//	int s = 0;
+//		for (;n >= 1;n--) {
+//			s = s + n;
+//		}
+//		return s;
+//}
+//int main() {
+//	cout << sumOneToN(5) << endl;
+//	cout << sumOneToN(10);
+//}
