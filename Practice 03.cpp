7d06e0ebd132576e9ac079caead3e53de8fa4905
harmain27 <harmainnoor5166@gmail.com>
2025-07-10 00:32:24 +0500

    Create Practice 03.cpp

---
+++ Practice 03.cpp
@@ -0,0 +1,16 @@
+////Sum of digits of a number
+//#include<iostream>
+//using namespace std;
+//int sum(int n) {
+//	int digitsum = 0;
+//	for (;n > 0;) {
+//		int lastdigit = n % 10;
+//		n = n / 10;
+//		digitsum = digitsum + lastdigit;
+//	}
+//	return digitsum;
+//}
+//int main() {
+//	cout << sum(1234) << endl;
+//	return 0;
+//}
