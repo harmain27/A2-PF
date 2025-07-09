ee268ffa1984236c0a03e21b1d1fce35e4e7ee93
harmain27 <harmainnoor5166@gmail.com>
2025-07-10 00:32:20 +0500

    Create Practice 02.cpp

---
+++ Practice 02.cpp
@@ -0,0 +1,14 @@
+////Calculate N factorial
+//#include<iostream>
+//using namespace std;
+//int Factorial(int n) {
+//	int f = 1;
+//	for (int i = 1;i <= n;n--) {
+//		f = f * n;
+//	}
+//	return f;
+//}
+//int main() {
+//	cout << Factorial(7) << endl;
+//	return 0;
+//}
