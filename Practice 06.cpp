e7384583e447d9c84b82d125039b4fed01405875
harmain27 <harmainnoor5166@gmail.com>
2025-07-10 00:32:35 +0500

    Create Practice 06.cpp

---
+++ Practice 06.cpp
@@ -0,0 +1,22 @@
+////print prime from 2 to n
+//#include<iostream>
+//using namespace std;
+//void TwotoNprime(int n) {
+//	for (int i = 2;i <= n;i++) {
+//		bool isPrime = true;
+//		for (int j = 2;j * j <= i; j++) {
+//			if (i % j == 0)
+//				isPrime=false;
+//		}
+//		if(isPrime)
+//		cout << i << endl;
+//	}
+//}
+//int main() {
+//	int a;
+//	cout << "Enter a number\n";
+//	cin >> a;
+//	cout << "Prime numbers till " << a << " are:-\n";
+//	TwotoNprime(a);
+//	return 0;
+//}
